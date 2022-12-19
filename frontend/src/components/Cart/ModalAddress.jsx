166110fc63a1ab078832d25f6582b9c1b4eb9de2
bichtram67 <tramnguyen672001@gmail.com>
2022-12-19 23:11:08 +0700

    update address

---
+++ frontend/src/components/Cart/ModalAddress.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import useLocationForm from "./useLocationForm";
+import Select from "react-select";
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import '../../sass/cart/checkoutCart.scss'
+const ModalAddress = ({ showAddressItem, setShowAddressItem }) => {
+    const handleClickAddressItem = () => {
+        setShowAddressItem(false)
+    }
+    const {
+        state,
+        onCitySelect,
+        onDistrictSelect,
+        onWardSelect,
+        onSubmit
+      } = useLocationForm(false);
+    
+      const {
+        cityOptions,
+        districtOptions,
+        wardOptions,
+        selectedCity,
+        selectedDistrict,
+        selectedWard
+      } = state;
+    
+      return (
+        <Modal
+            open={showAddressItem}
+            onClose={handleClickAddressItem}
+            classNames={{
+                overlay: 'customOverlay',
+                modal: 'custom-modal-detail-order',
+            }}
+        >
+
+            <div className="form-address">
+                <form
+                    onSubmit={onSubmit}
+                    
+                    >
+                    <div className="select-group">
+                        <Select
+                        name="cityId"
+                        key={`cityId_${selectedCity?.value}`}
+                        isDisabled={cityOptions.length === 0}
+                        options={cityOptions}
+                        onChange={(option) => onCitySelect(option)}
+                        placeholder="Tỉnh/Thành"
+                        defaultValue={selectedCity}
+                        />
+
+                        <br />
+
+                        <Select
+                        name="districtId"
+                        key={`districtId_${selectedDistrict?.value}`}
+                        isDisabled={districtOptions.length === 0}
+                        options={districtOptions}
+                        onChange={(option) => onDistrictSelect(option)}
+                        placeholder="Quận/Huyện"
+                        defaultValue={selectedDistrict}
+                        />
+                        <br />
+                        <Select
+                        name="wardId"
+                        key={`wardId_${selectedWard?.value}`}
+                        isDisabled={wardOptions.length === 0}
+                        options={wardOptions}
+                        placeholder="Phường/Xã"
+                        onChange={(option) => onWardSelect(option)}
+                        defaultValue={selectedWard}
+                        />
+
+                        <input type="text" placeholder='Nhập địa chỉ cụ thể' />
+                    </div>
+
+                    <button
+                        type="submit"
+                        
+                    >
+                        Xác nhận
+                    </button>
+                    </form>
+            </div>
+        </Modal >
+      )
+}
+
+export default ModalAddress
