6811df74801573faa500cb392b03603fb6475c42
unknown <quynhnhutg6@gmail.com>
2022-12-20 20:45:32 +0700

    up

---
+++ .history/frontend/src/components/Accessories/MainAccessories_20221220180519.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import TopSlider from '../Tablet/TopSlider'
+import { AccessoryContext } from '../../context/accessoryContext'
+import FeaturedListCate from './FeaturedListCate'
+import ListBrand from './ListBrand'
+import ListBlockAccessories from './ListBlockAccessories'
+import BlockSaleProduct from '../SubComponents/BlockSaleProduct'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProductCate, resetListCate } from '../../redux/Product/productSlice'
+import { useParams } from 'react-router-dom'
+
+
+const MainAccessories = () => {
+
+    const context = useContext(AccessoryContext)
+    const {listProductCate} = useSelector(state => state.product)
+  const dispatch = useDispatch()
+  const {id} = useParams()
+  useEffect(() => {
+    dispatch(resetListCate())
+    dispatch(getProductCate(id))
+  },[])
+
+  const listSale = (list_product) => {
+      return list_product.filter(val => val.promotion >= 50)
+  }
+  const 
+
+  return (
+    <div className='main-accessories'>
+
+        <div className="bg-slider">
+            <div className="container">
+                <TopSlider sliders={context.sliderTablet} banners={context.bannerTablet} />
+            </div>
+        </div>
+
+
+        <div className="container">
+
+            <FeaturedListCate />
+            <ListBrand />
+
+            <BlockSaleProduct listProduct={listProductCate} />
+
+           <ListBlockAccessories listProduct={listProductCate} />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default MainAccessories
