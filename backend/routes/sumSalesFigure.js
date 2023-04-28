e3ecac8efe934835f07c6421961cb24d81107fd4
unknown <quynhnhutg6@gmail.com>
2023-04-28 16:34:48 +0700

    up

--- backend/routes/sumSalesFigure.js
+++ backend/routes/sumSalesFigure.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
-const { getSummarySales, sumSales }  = require('../controllers/sumSalesFigureController')
+const { getSummarySales, sumSales, selectSumSales }  = require('../controllers/sumSalesFigureController')
 
 
 router.route('/sum-sales').get(getSummarySales)
 router.route('/add-sum-sales').post(sumSales)
+router.route('/select-sum-sales').post(selectSumSales)
 
 module.exports = router
