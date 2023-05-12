5ff112e965c0107d7820142916a35ca0542ce3a2
unknown <quynhnhutg6@gmail.com>
2023-05-12 13:36:24 +0700

    updating

--- backend/controllers/salesCategoryController.js
+++ backend/controllers/salesCategoryController.js
@@ -1,3 +1,4 @@
+const salesCategory = require('../models/salesCategory')
 const SalesCate = require('../models/salesCategory')
 
 
@@ -93,6 +94,36 @@ exports.salesCate = async ( req, res) => {
     res.status(201).json({
         success: true,
         sales_cate
+    })
+
+}
+
+
+exports.selectSalesCate = async (req,res) => {
+
+    const { year, month} = req.body
+
+    const sale_cates = await salesCategory.findOne({
+        year,
+        month
+    })
+
+    
+
+    if(!sale_cates)
+    {
+        res.status(401).json({
+            success:false,
+            message: 'Could not find sales category'
+        })
+
+        return 
+    }
+
+
+    res.status(201).json({
+        success: true,
+        sale_cates
     })
 
 }
--- backend/routes/salesCategory.js
+++ backend/routes/salesCategory.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 
-const { addSaleCate, salesCate, sale } = require('../controllers/salesCategoryController')
+const { addSaleCate, salesCate, sale, selectSalesCate } = require('../controllers/salesCategoryController')
 
 
 router.route('/sales-cate/add').post(addSaleCate)
-router.route('/sales-cate').get(salesCate)
+router.route('/sales-cate-year').post(salesCate)
+router.route('/sales-cate-month').post(selectSalesCate)
 
 module.exports = router
