2b502e36249b950a5ec9056103dc43cbc47b2827
unknown <quynhnhutg6@gmail.com>
2023-06-06 16:48:07 +0700

    update

--- backend/routes/guestVoucher.js
+++ backend/routes/guestVoucher.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 
-const { useVoucher } = require('../controllers/guestVoucherController')
+const { useVoucher,getUserVoucher } = require('../controllers/guestVoucherController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 router.route('/use-voucher').post(isAuthenticatedUser,authorizeRoles('user'),useVoucher)
+router.route('/user-voucher').get(isAuthenticatedUser,authorizeRoles('user'),getUserVoucher)
 
 module.exports = router
